4615b0beacafa387a422f351ef66881ab8cc30e4
Alex Markov <langhorne05@gmail.com>
2024-03-29 09:31:33 +0200

    Create Bet.cs

---
+++ Entity_Relations Exercises/02.FootballBetting/P02_FootballBetting.Data.Models/Bet.cs
@@ -0,0 +1,30 @@
+﻿using P02_FootballBetting.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P02_FootballBetting.Data.Models;
+
+public class Bet
+{
+    [Key]
+    public int BetId { get; set; }
+
+    [Required]
+    public decimal Amount { get; set; }
+
+    public Prediction Prediction { get; set; }
+
+    public DateTime DateTime { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(User))]
+    public int UserId { get; set; }
+
+    public virtual User User { get; set; } = null!;
+
+    [Required]
+    [ForeignKey(nameof(Game))]
+    public int GameId { get; set; }
+
+    public virtual Game Game { get; set; } = null!;
+}
