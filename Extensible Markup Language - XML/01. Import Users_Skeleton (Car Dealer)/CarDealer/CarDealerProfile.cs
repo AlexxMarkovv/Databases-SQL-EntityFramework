37699bde6c6d915612d2564cbbd0ba7e70575681
Alex Markov <langhorne05@gmail.com>
2024-04-12 16:58:08 +0300

    Create CarDealerProfile.cs

---
+++ Extensible Markup Language - XML/01. Import Users_Skeleton (Car Dealer)/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Export;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Import
+            this.CreateMap<ImportSupplierDto, Supplier>();
+            this.CreateMap<ImportPartDto, Part>();
+            this.CreateMap<ImportCarDto, Car>();
+            this.CreateMap<ImportCustomerDto, Customer>();
+            this.CreateMap<ImportSaleDto, Sale>();
+
+            //Export
+            this.CreateMap<Car, ExportCarsBmwDto>();
+            this.CreateMap<Sale, ExportSalesPerCustomerDto>();
+            this.CreateMap<Supplier, ExportLocalSuppliersDto>();
+        }
+    }
+}
