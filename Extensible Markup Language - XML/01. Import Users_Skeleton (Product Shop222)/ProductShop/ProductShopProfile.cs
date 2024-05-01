8e0e73a071b632a760ada4a1dabe49f3e6a50d8d
Alex Markov <langhorne05@gmail.com>
2024-05-01 21:32:29 +0300

    Create ProductShopProfile.cs

---
+++ Extensible Markup Language - XML/01. Import Users_Skeleton (Product Shop222)/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using ProductShop.DTOs.Export;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<ImportUserDto, User>();
+            CreateMap<ImportProductDto, Product>();
+            CreateMap<ImportCategoryDto, Category>();
+            CreateMap<ImportCategoryProductDto, CategoryProduct>();
+
+            CreateMap<Product, ExportProductDto>();
+
+            CreateMap<Category, ExportCategoryDto>();
+                //.ForMember(d => d.AveragePrice, opt =>
+                //             opt.MapFrom(s => s.CategoryProducts
+                //                    .Average(cp => cp.Product.Price)))
+                //.ForMember(d => d.TotalRevenue, opt =>
+                //             opt.MapFrom(s => s.CategoryProducts
+                //                    .Sum(cp => cp.Product.Price)))
+                //.ForMember(d => d.Count, opt =>
+                //             opt.MapFrom(s => s.CategoryProducts.Count));
+        }
+    }
+}
