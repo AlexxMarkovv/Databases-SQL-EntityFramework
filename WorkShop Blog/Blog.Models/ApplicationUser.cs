79760d5c246917c3f7470345334f7ea5e46fc6fe
Alex Markov <langhorne05@gmail.com>
2024-10-18 22:05:56 +0300

    Workshop Blog

---
+++ WorkShop Blog/Blog.Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models
+{
+    public class ApplicationUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string UserName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Email { get; set; }
+    }
+}
---
+++ WorkShop Blog/Blog.Models/Article.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(1500)]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+    }
+}
